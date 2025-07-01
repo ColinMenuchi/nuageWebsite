6ef914da30b5afc19a8844e172be8e1ffbf9513d
Colin Menuchi <cdmenuchi@gmail.com>
2025-07-01 18:57:27 -0400

    Added a placehold subheader

--- alpha-nuage-website/src/components/Header.jsx
+++ alpha-nuage-website/src/components/Header.jsx
@@ -5,7 +5,6 @@ function Header() {
     return(
         <div>
             <img className='header' src={banner} alt='NUAGE Banner'></img>
-            <h1 className='header'>NUAGE</h1>
         </div>
     );
 }
--- alpha-nuage-website/src/components/Subheader.jsx
+++ alpha-nuage-website/src/components/Subheader.jsx
@@ -3,7 +3,23 @@ function Subheader() {
 
     return(
         <div>
-            <h3 className='subheader'>Northeastern University's student led association of game enthusiasts!</h3>
+            <h3 className='subheader'>Welcome to NUAGE, Northeastern University's
+                 student led board game club!
+            </h3>
+            <h5>
+                Northeastern University Association of Gaming Enthusiasts
+                (NUAGE) is Northeastern’s official tabletop gaming club. We hold Casual Game Night and
+                 other events every Friday and Saturday night starting at 7:00PM in Forsyth building room
+                 #201. We’ve got tons of board games for everyone to enjoy, so don’t hesitate to come out
+                 to meet some new people and learn some new games. Never heard of any of our games? No 
+                 problem! We at NUAGE love teaching games almost as much as we love playing them.
+                 NUAGE will stimulate students’ minds through puzzle and logic based thought in the form
+                 of fun and enriching games. We have no target student demographic and welcome all those 
+                 interested in playing a wide variety of games. Our mission is to create a diverse 
+                 community of Northeastern Students interested in puzzle and logic based board games. 
+                 The goal isn’t to win, but to have fun; although, winning is nice.
+            </h5>
+
         </div>
     );
 }
