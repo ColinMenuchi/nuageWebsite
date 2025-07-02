b530719e46e8cc7aa72cd8cd206304a5045585e8
Colin Menuchi <cdmenuchi@gmail.com>
2025-07-02 14:12:28 -0400

    Made initial styling for cards

--- alpha-nuage-website/src/App.jsx
+++ alpha-nuage-website/src/App.jsx
@@ -4,6 +4,8 @@ import Subheader from './components/Subheader.jsx'
 import Card from './components/Card.jsx'
 
 import calendar from './assets/calendar.jpeg'
+import boardGameCollection from './assets/boardGameCollection.jpeg'
+import lightbulb from './assets/lightbulb.jpeg'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <Header></Header>
       <Subheader></Subheader>
       <Card image={calendar} title="Upcoming Events" text="Discover Our Events!"></Card>
+      <Card image={boardGameCollection} title="Board Games" text="Explore Our Collection!"></Card>
+      <Card image={lightbulb} title="Leadership & eBoard" text="Meet Our Leaders!"></Card>
     </>
   )
 }
