987167d2cb89db9f3eefbc75fe950007ccea8e17
Colin Menuchi <cdmenuchi@gmail.com>
2025-08-22 11:56:45 -0400

    Added a card to the About Us Page. Realized I need to make card subclasses.

--- alpha-nuage-website/src/components/About.jsx
+++ alpha-nuage-website/src/components/About.jsx
@@ -1,9 +1,19 @@
+import nuageBanner from '../assets/nuageBanner.jpg'
+import Card from './Card.jsx'
 
 function About() {
 
     return(
     <div>
-        <h1>Meet Our Team</h1>
+        <h1 style={{
+            marginLeft: "50px",
+            marginTop: "30px",
+            fontFamily: "papyrus",
+        }}
+        >
+            Meet Our Team:
+        </h1>
+        <Card image={nuageBanner} title="Leader's Name" text="Text About Them"></Card>
     </div>
     );
 }
