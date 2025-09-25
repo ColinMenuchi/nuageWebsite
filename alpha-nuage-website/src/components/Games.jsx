46b1b54d436cee6aa20d6b616ee40f3c05a0a632
Colin Menuchi <cdmenuchi@gmail.com>
2025-09-25 17:28:04 -0400

    Hardcoded display for Games page before trying to switch to a more dynamic data based design.

--- alpha-nuage-website/src/components/Games.jsx
+++ alpha-nuage-website/src/components/Games.jsx
@@ -1,13 +1,30 @@
-
+import Card from "./Card.jsx"
+import nuageBanner from "../assets/nuageBanner.jpg"
+import botc from "../assets/botc.webp"
+import cosmicEncounter from "../assets/cosmicEncounter.jpeg"
+import forestShuffle from "../assets/forestShuffle.webp"
+import fromage from "../assets/fromage.jpeg"
+import slayTheSpire from "../assets/slayTheSpire.jpeg"
+import spiritIsland from "../assets/spiritIsland.webp"
 
 function Games() {
 
     return(
     <div>
-        <dl>Hi</dl>
-        <dd>This says hi</dd>
-        <dl>Hi</dl>
-        <dd>So does this</dd>
+        <h1 style={{
+            marginLeft: "50px",
+            marginTop: "30px",
+            fontFamily: "papyrus",
+        }}
+        >
+            Browse Our Game Collection:
+        </h1>
+        <Card image={botc} title="Blood on the Clocktower" text="Text About Them"></Card>
+        <Card image={cosmicEncounter} title="Cosmic Encounter" text="Text About Them"></Card>
+        <Card image={forestShuffle} title="Forest Shuffle" text="Text About Them"></Card>
+        <Card image={fromage} title="Fromage" text="Text About Them"></Card>
+        <Card image={slayTheSpire} title="Slay the Spire" text="Text About Them"></Card>
+        <Card image={spiritIsland} title="Spirit Island" text="Text About Them"></Card>
     </div>);
 
 }
