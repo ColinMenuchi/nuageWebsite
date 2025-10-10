88e393cae3cfaec0433e843a9df24799d1fa6042
Colin Menuchi <cdmenuchi@gmail.com>
2025-10-10 13:39:49 -0400

    Added images for games on top shelf F

--- alpha-nuage-website/src/GamesDB.jsx
+++ alpha-nuage-website/src/GamesDB.jsx
@@ -6,12 +6,42 @@ import slayTheSpire from "./assets/gamePhotos/slayTheSpire.jpeg";
 import spiritIsland from "./assets/gamePhotos/spiritIsland.webp";
 
 const games_database = [
-    { image: botc, title: "Blood on the Clocktower", description: "TBA"},
-    { image: cosmicEncounter, title: "Cosmic Encounter", description: "TBA"},
-    { image: forestShuffle, title: "Forest Shuffle", description: "TBA"},
-    { image: fromage, title: "Fromage", description: "TBA"},
-    { image: slayTheSpire, title: "Slay the Spire", description: "TBA"},
-    { image: spiritIsland, title: "Spirit Island", description: "TBA"},
+    { 
+        image: botc, 
+        title: "Blood on the Clocktower", 
+        description: "TBA",
+        storageLocation: "3rd Shelf Locker G-Right"
+    },
+    { 
+        image: cosmicEncounter, 
+        title: "Cosmic Encounter", 
+        description: "TBA",
+        storageLocation: "Bottom Shelf Locker G-Left"
+    },
+    { 
+        image: forestShuffle, 
+        title: "Forest Shuffle", 
+        description: "TBA",
+        storageLocation: "Drawers Locker G-Right"
+    },
+    { 
+        image: fromage, 
+        title: "Fromage", 
+        description: "TBA",
+        storageLocation: "Top Shelf Locker B"
+    },
+    { 
+        image: slayTheSpire, 
+        title: "Slay the Spire", 
+        description: "TBA",
+        sotrageLocation: "3rd Shelf Locker B"
+    },
+    { 
+        image: spiritIsland, 
+        title: "Spirit Island", 
+        description: "TBA",
+        storageLocation: "3rd Shelf Locker B",
+    },
 ]
 
 export default games_database;
