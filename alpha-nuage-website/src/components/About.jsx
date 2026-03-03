6163d7a2e8dde947279dea0aaa41154f56998f6e
Colin Menuchi <cdmenuchi@gmail.com>
2026-03-03 18:10:30 -0500

    added eBoard photos to about page

--- alpha-nuage-website/src/components/About.jsx
+++ alpha-nuage-website/src/components/About.jsx
@@ -13,7 +13,12 @@ function About() {
         >
             Meet Our Team:
         </h1>
-        <AboutPopUp imagePath='./src/assets/businessWoman.jpeg' title="Jane Doe" cardText="Club Secretary and Gravwell Enthusiast" popUpText="Idk man..... :/"></AboutPopUp>
+        <AboutPopUp imagePath='./src/assets/eBoardPhotos/kimberlyTai.jpeg' title="Kimberly Tai" cardText="Club President and Forest Shuffle Enthusiast" popUpText="Idk man..... :/"></AboutPopUp>
+        <AboutPopUp imagePath='./src/assets/eBoardPhotos/oscarAdler.jpeg' title="Oscar Adler" cardText="Club Vice President and Ark Nova Enjoyer" popUpText="Idk man..... :/"></AboutPopUp>
+        <AboutPopUp imagePath='./src/assets/eBoardPhotos/colinTroan.jpeg' title="Colin Troan" cardText="Club Treasurer and Slay the Spire Fanatic" popUpText="Idk man..... :/"></AboutPopUp>
+        <AboutPopUp imagePath='./src/assets/eBoardPhotos/mattDavis.jpeg' title="Matt Davis" cardText="Club Secretary and Spirit Island Addict" popUpText="Idk man..... :/"></AboutPopUp>
+        <AboutPopUp imagePath='./src/assets/eBoardPhotos/ellieHenderson.jpeg' title="Ellie Henderson" cardText="Club Game Master and Harmonies Connoissuer" popUpText="Idk man..... :/"></AboutPopUp>
+        <AboutPopUp imagePath='./src/assets/eBoardPhotos/colinMenuchi.jpeg' title="Colin Menuchi" cardText="Club PR Director and Chess Grandmaster" popUpText="Idk man..... :/"></AboutPopUp>
         <Card imagePath='./src/assets/nuageBanner.jpg' title="Leader's Name" text="Text About Them"></Card>
         <Card imagePath='./src/assets/nuageBanner.jpg' title="Leader's Name" text="Text About Them"></Card>
         <Card imagePath='./src/assets/nuageBanner.jpg' title="Leader's Name" text="Text About Them"></Card>
