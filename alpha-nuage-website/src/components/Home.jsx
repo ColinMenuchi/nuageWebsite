714bc8483b91ed4a413c45f679841486962ed1aa
Colin Menuchi <cdmenuchi@gmail.com>
2026-03-24 19:58:02 -0400

    Commented out cards

--- alpha-nuage-website/src/components/Home.jsx
+++ alpha-nuage-website/src/components/Home.jsx
@@ -1,11 +1,18 @@
 import TopMenu from './TopMenu.jsx'
+import Header from './Header.jsx'
 import Subheader from './Subheader.jsx'
+import Card from './Card.jsx'
 
 function Home() {
 
   return (
     <>
       <Header></Header>
+      <Subheader></Subheader>
+      
+      { /* <Card imagePath='./src/assets/calendar.jpeg' title="Upcoming Events" text="Discover Our Events!"></Card>
+      <Card imagePath='./src/assets/boardGameCollection.jpeg' title="Board Games" text="Explore Our Collection!"></Card>
+      <Card imagePath='./src/assets/lightbulb.jpeg' title="Leadership & eBoard" text="Meet Our Leaders!"></Card> */}
     </>
   )
 }
